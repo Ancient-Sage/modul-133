9a570c9ca98a931937e28018e6790825ffea77d7
Marco Schmalz <marco@schess.ch>
2019-01-10 11:10:48 +0100

    Simple backend

---
+++ js/Werkstatt-Sessions/03-Simple-Backend/server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+
+var datenbank = [
+  {id: 0, username: 'root', password: 'admin'}
+]
+var idCounter = 1
+
+app.get('/api/users/', (request, response) => {
+  response.json(datenbank)
+})
+
+app.post('/api/users/', (request, response) => {
+  console.log(request.body)
+  var newUser = {
+    id: idCounter,
+    username: request.body.username,
+    password: request.body.password
+  }
+  idCounter += 1
+  datenbank.push(newUser)
+  response.send('ok')
+})
+
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
